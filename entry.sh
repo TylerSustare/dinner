974a4f62fd3a76cb3759a8c8caf535bf22aecdb4
Tyler Sustare <tylersutare@gmail.com>
2020-10-27 10:35:03 -0700

    deploy with GCR

--- entry.sh
+++ entry.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $PORT ]; then
-    bundle exec rails server -b 0.0.0.0 -p $PORT -e production
+    bundle exec rails server -b 0.0.0.0 -e production -p $PORT
 else
-    bundle exec rails server -b 0.0.0.0 -p 3000 -e production
+    bundle exec rails server -b 0.0.0.0 -e production -p 3000
 fi
 # rails server -b 0.0.0.0 -p $PORT -e production
